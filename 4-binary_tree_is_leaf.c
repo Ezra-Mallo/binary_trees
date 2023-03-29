2336128e912162c58b4ab260b2b257505d34174b
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-29 11:08:10 +0100

    Task 3. Delete

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - the memory address of the node
+ * @node: Value of the node
+ * Return: Returns an int value
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node->right == NULL && node->left == NULL)
+		return (1);
+	else
+		return (0);
+}
